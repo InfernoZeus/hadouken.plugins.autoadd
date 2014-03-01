1c7f09b88b401b7a83ee6ed08f02b46b4dd641bd
Viktor Elofsson <viktor@viktorelofsson.se>
2014-03-01 21:07:39 +0100

    Basic setup for plugin development

--- src/Hadouken.Plugins.AutoAdd/AutoAddPlugin.cs
+++ src/Hadouken.Plugins.AutoAdd/AutoAddPlugin.cs
@@ -1,6 +1,12 @@
-﻿namespace Hadouken.Plugins.AutoAdd
+﻿using Hadouken.Fx;
+
+namespace Hadouken.Plugins.AutoAdd
 {
-    public class AutoAddPlugin
+    public class AutoAddPlugin : Plugin
     {
+        public override void Load()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
